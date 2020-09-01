3f1d4723b75d83afced5f6c7784e6ff454955cc5
Sharon Paz <oxleberry@gmail.com>
2020-09-01 12:30:42 -0700

    Adding styles to Product Cards on homepage and product details page

--- src/components/product.js
+++ src/components/product.js
@@ -3,22 +3,43 @@ import React from "react"
 import Img from 'gatsby-image'
 
 const Product = ({ image, name, price, desc, sizes }) => (
-  <div className="product-item-container">
-    <Img
-      fluid={image}
-      alt={name}
-      style={{ maxWidth: '300px' }}
-    />
-    <div className="product-item-details">
-      <h3 className="text-3xl text-purple-600">{name}</h3>
-      <span>$ </span>
-      <span>{price}</span>
-      <p>{desc}</p>
-      <ul>
-        {sizes.map(size => (
-          <li>{size}</li>
-        ))}
-      </ul>
+  <div className="flex mx-12 pb-12">
+    {/* Image, Description */}
+    <div className="w-1/2 border-2 border-orange-200 mr-8">
+      <div className="p-12">
+        <Img
+          fluid={image}
+          alt={name}
+        />
+      </div>
+      <p className="text-gray-700 bg-orange-100 border-t-2 border-orange-200 p-4">{desc}</p>
+    </div>
+
+    <div className="flex flex-col w-1/2">
+      {/* Title, Price */}
+      <h3 className="text-4xl text-gray-700">{name}</h3>
+      <div className="font-semibold text-gray-700 py-4">
+        <span>$ </span>
+        <span>{price}</span>
+      </div>
+      {/* Size dropdown */}
+      <div className="dropdown block relative">
+        <button className="flex justify-between items-center w-full text-gray-700 border-2 border-gray-300 px-4 py-1">
+          <span>Size</span>
+          <svg className="w-4 h-4" viewBox="0 0 20 10">
+          <polyline points="2,2 10,10 18,2" stroke="grey" strokeWidth="2" strokeLinecap="round" strokeLinejoin="milter" fill="none"/>
+        </svg>
+        </button>
+        <ul className="dropdown-menu hidden">
+          {sizes.map(size => (
+            <li>{size}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Add to cart, Key features */}
+      <button className="font-semibold text-gray-800 bg-orange-300 border-2 border-orange-300 my-5 p-1">Add to Cart</button>
+      <p className="font-bold text-gray-700 pt-4 pb-4">Key Features:</p>
+      <p className="text-gray-700">REGULAR: Height 5.5" x Width 4.4"<br /> MINI: Height 3" x Width 2.4"<br /><br /> Gummi bears biscuit pudding apple pie bonbon dessert tiramisu gummies gingerbread. Caramels toffee wafer toffee cake marshmallow carrot cake croissant cupcake. Biscuit gummies dessert dessert. Chocolate carrot cake sweet dragée. </p>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,34 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 import "../styles/main.scss"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="text-3xl text-blue-600">List of Products</h1>
-    <ul>
-      {data.allContentfulProduct.edges.map(({ node, index }) => (
-        <li key={index}>
-          <Link className="text-orange-600" to={`/product/${node.slug}`}>{node.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1 className="text-center text-3xl text-gray-700 py-4">Animated Sticker Page</h1>
+      <ul className="grid grid-cols-4 gap-4 pb-12">
+        {data.allContentfulProduct.edges.map(({ node }, idx) => (
+          <li className="border-2 border-orange-200" key={idx}>
+            <Link to={`/product/${node.slug}`}>
+              <Img className="m-3"
+                fluid={node.image.fluid}
+                alt={node.name}
+              />
+              <div className="text-center text-gray-700 bg-orange-100 border-orange-200 border-t-2 p-3">
+                <p className="text-l">{node.name}</p>
+                <span>$ </span>
+                <span>{node.price}</span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   </Layout>
 )
 
@@ -29,6 +41,12 @@ export const query = graphql`
         node {
           name
           slug
+          price
+          image {
+            fluid(maxWidth: 400) {
+              ...GatsbyContentfulFluid_noBase64
+            }
+          }
         }
       }
     }
