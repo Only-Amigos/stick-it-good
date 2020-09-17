b3fd9ce5f3f6e600821cad7b288b2f0a1cd9a2d9
Sharon Paz <oxleberry@gmail.com>
2020-09-17 13:13:57 -0700

    Adding PurgeCSS plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-purgecss`, // Must be placed after Sass & PostCss plugins
+      options: {
+        printRejected: false, // Print removed selectors and processed file names
+        develop: false, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
